4897537d1bf72effd6938e6cb11e676b57bb3074
appleseedexm <appleseedexm@gmail.com>
2024-06-26 09:46:51 +0200

    improved test method names

--- app/src/test/java/org/example/app/MessageUtilsTest.java
+++ app/src/test/java/org/example/app/MessageUtilsTest.java
@@ -8,14 +8,14 @@ import org.junit.jupiter.api.Test;
 class MessageUtilsTest {
 
   @Test
-  void test_ConflictingClass_fromTest_shouldFail() {
+  void test_ConflictingClass_fromTest_shouldNotBeEqual() {
     var conflict = new ConflictingClass();
 
     assertNotEquals("test", conflict.getMessage());
   }
 
   @Test
-  void test_ConflictingClass_fromTestFixtures() {
+  void test_ConflictingClass_fromTestFixtures_shouldBeEqual() {
     var conflict = new ConflictingClass();
 
     assertEquals("testFixtures", conflict.getMessage());
